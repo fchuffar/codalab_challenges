a4c9966837596ce3e5de6a27386cedface08d197
Florent Chuffart <florent.chuffart@gmail.com>
2019-04-08 21:18:17 +0200

    Refactoring sk

--- results/challenge_expred/scoring_program/scoring.r
+++ results/challenge_expred/scoring_program/scoring.r
@@ -30,89 +30,118 @@ if (ADMIN_SESSION) {
   # Ground truth
   d_full = readRDS(data_full_filename)
   gs = colnames(d_full)[11:31]
-  d_full = as.matrix(d_full[,gs])
+  dg_full = as.matrix(d_full[,gs])
 
   # get NA  
   nb_na = 20000
-  gr = expand.grid(1:nrow(d_full), 1:length(gs))
+  gr = expand.grid(1:nrow(dg_full), 1:length(gs))
   set.seed(1) 
   idx = sample(1:nrow(gr), nb_na)
-  na_coord = data.frame(sample=rownames(d_full)[gr[idx,1]], gene=gs[gr[idx,2]], stringsAsFatcors=FALSE)
+  na_coord = data.frame(sample=rownames(dg_full)[gr[idx,1]], gene=gs[gr[idx,2]], stringsAsFactors=FALSE)
 
-  # generate seed with the secret (d_full) 
-  set.seed(round(sum(d_full[,ncol(d_full)])))
+  # generate seed with the secret (dg_full) 
+  set.seed(round(sum(dg_full[,ncol(dg_full)])))
   idx_r2 = sample(1:nrow(na_coord), ceiling(nb_na/2))
   na_coord = na_coord[idx_r2,]
   rownames(na_coord) = NULL
 
   # Load submited results from participant
-  pred_full = readRDS(paste0(input, res, "results.rds"))
+  predg_full = readRDS(paste0(input, res, "results.rds"))
 
   # Define scoring function
-  score = function(truth, pred, output_file, prefix){
+  score = function(truth, pred, prefix=NULL){
     nbna = sum(is.na(pred))
     propNA = nbna/length(truth)
+
     lm = lm(truth~pred)
     res = residuals(lm)
     mse = mean(res^2)
-    sc = list(MSE=mse, propNA=nbna/length(truth), nbNA=nbna)
-    if (!missing(prefix)) {
+    sc = list(propNA=nbna/length(truth), MSE=mse)
+    if (!is.null(prefix)) {
       names(sc) = paste(names(sc), prefix, sep="_")
     }
-      
-    cat(sprintf("n: %f\n", length(truth)), file=output_file, append=FALSE)      
-    foo = sapply(names(sc), function(key){
-      cat(sprintf(paste(key, ":%f\n", sep=""), sc[[key]]), file=output_file, append=TRUE)      
-    })
-    cat(readLines(output_file), sep = "\n") 
-
-    # cat(sprintf("MSE:   %f\n", mse  ), file=output_file, append=FALSE)
-    # cat(sprintf("nbNA:  %f\n", nbna ), file=output_file, append=TRUE)
-    # cat(readLines(output_file), sep = "\n")
-    #
-    #
-    # cat(sprintf("MSE:   %f\n", mse  ), file=output_file, append=FALSE)
-    # cat(sprintf("propNA:  %f\n", )), file=output_file, append=TRUE)
-    # cat(sprintf("nbNA:  %f\n", nbna ), file=output_file, append=TRUE)
     return(sc)
   }
+
+  names(gs)=gs
+  proj = unique(d_full$project)
+  names(proj) = proj  
+  proj = rev(sort(sapply(proj, function(pr) {nrow(na_coord[d_full[na_coord$sample,]$project %in% pr,])})))
+  proj = proj[proj>0]
+  proj = names(proj)
+  names(proj) = substr(proj, 6, 1000)  
+
+  sub_na_coord = list(
+    list(GLOBAL=na_coord),
+    list(NORMAL=na_coord[d_full[na_coord$sample,]$tissue_status=="normal",]),
+    lapply(gs, function(g) {na_coord[na_coord$gene==g,]}),
+    lapply(proj, function(pr) {na_coord[d_full[na_coord$sample,]$project %in% pr,]})
+  )
+
+  length(sub_na_coord)
+  sub_na_coord = unlist(sub_na_coord, recursive=FALSE)
+  length(sub_na_coord)
+  sapply(sub_na_coord, nrow)
+
   
-  # Evaluate prediction
-  pred = apply(na_coord, 1, function(l) {
-    pred_full[l[["sample"]], l[["gene"]]]
+  scores = sapply(names(sub_na_coord), function(n){
+    print(n)
+    pred = apply(sub_na_coord[[n]], 1, function(l) {
+      predg_full[l[["sample"]], l[["gene"]]]
+    })
+    truth = apply(sub_na_coord[[n]], 1, function(l) {
+      dg_full[l[["sample"]], l[["gene"]]]
+    })
+    score(truth, pred)
   })
 
-  truth = apply(na_coord, 1, function(l) {
-    d_full[l[["sample"]], l[["gene"]]]
-  })
-  foo = score(truth, pred, output_file=paste0(output,"/scores.txt"))
-  
-  # R2 R3 R4
-  # un score global
-  # Un score par gène
-  # Nb NA
-  # Un score sur normal
-  # Un score sur tumoral
-  # Un score sur homme
-  # Un score sur femme
-  # Un score par projet
+  write_scores = function(scores, output_file) {
+    for (grp in colnames(scores)) {
+      for (ind in rownames(scores)) {
+        key = paste(grp, ind, sep="_")
+        cat(sprintf(paste(key, ":%f\n", sep=""), scores[ind,grp]), file=output_file, append=!(grp==colnames(scores)[1]&ind==rownames(scores)[1]))              
+      }
+    }
+    cat(readLines(output_file), sep = "\n")     
+  }
 
-  foo = sapply(gs, function(g){
-    sub_na_coord = na_coord[na_coord$gene==g,] 
-    pred = apply(sub_na_coord, 1, function(l) {
-      pred_full[l[["sample"]], l[["gene"]]]
-    })
-    truth = apply(sub_na_coord, 1, function(l) {
-      d_full[l[["sample"]], l[["gene"]]]
-    })
-    # score(truth, pred, output_file=paste0(output,"/scores.txt"), prefix=g)
-  })
+  write_scores(scores, output_file=paste0(output,"/scores.txt"))
 }
 
 
 # export codallab bundle (if local run with reference data, it means if admin run)
 if (ADMIN_SESSION & CHALLENGER_SESSION) {
   print("Export codallab bundle, beause of (CHALLENGER_SESSION & ADMIN_SESSION).")
+
+  write_board = function(scores) {
+    output_file="board.yml"
+    i = 1
+        cat(paste("leaderboard:                                        \n" , sep=""), file=output_file, append=FALSE)              
+        cat(paste("  columns:                                          \n" , sep=""), file=output_file, append=TRUE)              
+    for (grp in colnames(scores)) {
+      for (ind in rownames(scores)) {
+        key = paste(grp, ind, sep="_")
+        FIRST_ELEMENT = grp==colnames(scores)[1]&ind==rownames(scores)[1]
+        cat(paste("    ", key, ":                                      \n" , sep=""), file=output_file, append=TRUE)              
+        cat(paste("      label: ", key, "                              \n" , sep=""), file=output_file, append=TRUE)              
+        cat(paste("      leaderboard: ", ifelse(FIRST_ELEMENT,"&","*"), "id001\n" , sep=""), file=output_file, append=TRUE)              
+        if (FIRST_ELEMENT) {
+
+        cat(paste("        label: ", key, "                            \n" , sep=""), file=output_file, append=TRUE)              
+        cat(paste("        rank: 1                                     \n" , sep=""), file=output_file, append=TRUE)              
+        }        
+        cat(paste("      rank: ", i, "                                 \n" , sep=""), file=output_file, append=TRUE)              
+        cat(paste("      sort: asc                                     \n" , sep=""), file=output_file, append=TRUE)              
+        i = i+1
+      }
+    }
+        cat(paste("  leaderboards:           \n" , sep=""), file=output_file, append=TRUE)              
+        cat(paste("    GLOBAL_MSE: *id001    \n" , sep=""), file=output_file, append=TRUE)              
+        cat(readLines(output_file), sep = "\n")     
+  }
+  write_board(scores)
+
+
   zip_filename = "reference_data.zip"
   zip(zip_filename, "data_full.rds")
 
